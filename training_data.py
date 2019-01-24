dd40418cf883b1745cc22b1ba8681f49e707be4e
ph <lipshitc@gmail.com>
2019-01-24 23:16:18 +0200

    Initial commit

---
+++ training_data.py
@@ -0,0 +1,31 @@
+# based on deepfakes sample project
+# https://github.com/deepfakes/faceswap
+
+import numpy
+from image_augmentation import random_transform
+from image_augmentation import random_warp
+
+random_transform_args = {
+    'rotation_range': 10,
+    'zoom_range': 0.05,
+    'shift_range': 0.05,
+    'random_flip': 0.4,
+    }
+
+# next batch of our train data
+def get_training_data( images, batch_size ):
+    indices = numpy.random.randint( len(images), size=batch_size )
+    for i,index in enumerate(indices):
+        image = images[index]
+        image = random_transform( image, **random_transform_args )
+        warped_img, target_img = random_warp( image )
+
+        # first index => initiate array
+        if i == 0:
+            warped_images = numpy.empty( (batch_size,) + warped_img.shape, warped_img.dtype )
+            target_images = numpy.empty( (batch_size,) + target_img.shape, warped_img.dtype )
+
+        warped_images[i] = warped_img
+        target_images[i] = target_img
+
+    return warped_images, target_images
